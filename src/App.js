e0340d02ac83869f0bcff50a15d6eca3a84ae7ab
hashamuddin1 <hashamlaptop@gmail.com>
2024-06-08 03:36:57 +0500

    get all user done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import User from "./components/User";
-import List from "./components/List";
-import Policy from "./components/Policy";
-import Issue from "./components/Issue";
 
 function App() {
   return (
@@ -14,9 +11,6 @@ function App() {
           <Route path="/" exact element={<Home />}></Route>
           <Route path="/login" exact element={<Login />}></Route>
           <Route path="/user" exact element={<User />}></Route>
-          <Route path="/list" exact element={<List />}></Route>
-          <Route path="/policy" exact element={<Policy />}></Route>
-          <Route path="/issue" exact element={<Issue />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/User.js
+++ src/components/User.js
@@ -5,7 +5,6 @@ import Navbar from "./Navbar";
 import { DataGrid } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom";
 import Skeleton from "@mui/material/Skeleton";
-import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 import "../App.css";
 
@@ -13,33 +12,11 @@ export default function User() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [success, setSuccess] = useState(null);
   const navigate = useNavigate();
   const columns = [
     { field: "fullName", headerName: "Full Name", width: 200 },
     { field: "emailAddress", headerName: "Email Address", width: 250 },
-    { field: "phoneNumber", headerName: "Phone Number", width: 200 },
-    { field: "state", headerName: "State", width: 200 },
-    { field: "city", headerName: "City", width: 200 },
-    {
-      field: "delete",
-      headerName: "Delete",
-      headerAlign: "center",
-      width: 100,
-      renderCell: (params) => (
-        <Button
-          onClick={(e) => {
-            e.stopPropagation();
-            handleDeleteClick(params.row.id);
-          }}
-          className="deletedBtn"
-          variant="contained"
-          sx={{ backgroundColor: "red" }}
-        >
-          Delete
-        </Button>
-      ),
-    },
+    { field: "role", headerName: "Role", width: 250 },
   ];
 
   const SkeletonTable = () => (
@@ -52,30 +29,6 @@ export default function User() {
     </div>
   );
 
-  const handleDeleteClick = async (userId) => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axios.delete(
-        `${process.env.REACT_APP_BASE_URL_V1}/api/deleteUser`,
-        { headers: { "x-access-token": `${token}` }, params: { userId } }
-      );
-
-      if (response.data) {
-        setData((prevData) => prevData.filter((user) => user._id !== userId));
-        showSuccessModal(response.data.message);
-      }
-    } catch (error) {
-      console.error("Delete user error:", error);
-    }
-  };
-
-  const showSuccessModal = (successMessage) => {
-    setSuccess(successMessage);
-    setTimeout(() => {
-      setSuccess(null);
-    }, 3000);
-  };
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -85,7 +38,7 @@ export default function User() {
       try {
         setIsLoading(true);
         const response = await axios.get(
-          `${process.env.REACT_APP_BASE_URL_V1}/api/fetchAllUser`,
+          `${process.env.REACT_APP_BASE_URL_V1}/api/getAllUsers`,
           {
             headers: {
               "x-access-token": `${token}`,
@@ -120,50 +73,43 @@ export default function User() {
     );
   }
 
-  const rowsWithIds = data.map(
-    ({ _id, fullName, emailAddress, phoneNumber, state, city }) => ({
+  if (data) {
+    const rowsWithIds = data.map(({ _id, fullName, emailAddress, role }) => ({
       fullName,
       emailAddress,
-      phoneNumber,
-      state,
-      city,
+      role,
       id: _id,
-    })
-  );
+    }));
 
-  return (
-    <>
-      <Box sx={{ display: "flex" }}>
-        <Navbar />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          {error && (
-            <Alert variant="filled" severity="error">
-              {error}
-            </Alert>
-          )}
-          {success && (
-            <Alert variant="filled" severity="success">
-              {success}
-            </Alert>
-          )}
-          <h3>All Users</h3>
-          <div style={{ height: 450, width: "100%" }}>
-            <DataGrid
-              rows={rowsWithIds}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: { page: 0, pageSize: 10 },
-                },
-              }}
-              pageSizeOptions={[2, 5, 10]}
-              checkboxSelection={false}
-              autoHeight
-              autoHeightSizeMode="fullWidth"
-            />
-          </div>
+    return (
+      <>
+        <Box sx={{ display: "flex" }}>
+          <Navbar />
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            {error && (
+              <Alert variant="filled" severity="error">
+                {error}
+              </Alert>
+            )}
+            <h3>All Users</h3>
+            <div style={{ height: 450, width: "100%" }}>
+              <DataGrid
+                rows={rowsWithIds}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                    paginationModel: { page: 0, pageSize: 10 },
+                  },
+                }}
+                pageSizeOptions={[2, 5, 10]}
+                checkboxSelection={false}
+                autoHeight
+                autoHeightSizeMode="fullWidth"
+              />
+            </div>
+          </Box>
         </Box>
-      </Box>
-    </>
-  );
+      </>
+    );
+  }
 }
